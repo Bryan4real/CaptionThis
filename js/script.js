f6e97ba2d8ce0adda61dd9ee2eab61e118082b2d
DjCooGie <evansonchagwa01@gmail.com>
2019-11-13 13:26:42 +0300

    initial nodification to the files

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 $(document).ready(function () {
 
-  $("#in").click(function () {
+  $("#in").click(function (event) {
     $("#in").animate({ height: "fadeout", opacity: "toggle" }, "slow");
     $("#out").animate({ height: "fadeout", opacity: "toggle" }, "slow");
     $(".login-page").fadeIn(2000);
   })
 
-  
+
   $(".sms a").click(function () {
-    $("form").animate({ height: "toggle", opacity: "toggle"}, "slow");
+    $("form").animate({ height: "toggle", opacity: "toggle" }, "slow");
   })
 
 
@@ -18,6 +18,6 @@ $(document).ready(function () {
 
 
 
-
+  event.preventDefault();
 
 });
