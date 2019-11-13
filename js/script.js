f1abac764cbe417e1e95a7b093ed111b51c161b1
DjCooGie <evansonchagwa01@gmail.com>
2019-11-14 02:32:13 +0300

    added a form validation function. added inputs, paragraphs and h1 tags. modified the styling of the forms

--- js/script.js
+++ js/script.js
@@ -1,3 +1,38 @@
+
+function captionThis() { 
+    
+	let name = document.getElementById("usnam").value;
+	let email = document.getElementById("ema").value;
+  let password = document.getElementById("pass").value;
+  
+  
+	if (name == "") {
+    window.alert("Please enter your name.");
+  name.focus();
+  return false;
+}
+
+
+if (email == "") {
+    window.alert("Please enter a valid e-mail address.");
+  email.focus();
+  return false;
+}
+
+if (password == "") {
+    window.alert("Please enter your password");
+  password.focus();
+  return false;
+}
+
+return true;
+}
+
+
+
+
+
+  
 $(document).ready(function () {
 
   $("#in").click(function (event) {
@@ -10,6 +45,7 @@ $(document).ready(function () {
   $(".sms a").click(function () {
     $("form").animate({ height: "toggle", opacity: "toggle" }, "slow");
   })
+
 
 
 
