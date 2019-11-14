a7b88fe7eab94f37bba2b05815727aa4698bf8af
nessa-waithaka <vanessa.waithaka@gmail.com>
2019-11-14 14:28:49 +0300

    Fix upload button functionality

--- js/script.js
+++ js/script.js
@@ -1,31 +1,28 @@
 $(document).ready(function() {
-  const inpfile = document.getElementById("inpImage");
-  const imageContainer = document.getElementById("ImagePreview");
-  const imagePreview = document.getElementById("#image-field");
-  const previewText = document.getElementById(".image-text");
+  const inpfile = document.getElementById("inputImage");
+  // const imageContainer = document.getElementById("ImagePreview");
+  // const imagePreview = document.getElementById("#image-field");
 
-  if (file) {
-    const reader = new FileReader();
-    previewText.style.display = "none";
-    imagePreview.style.display = "block";
-    reader.addEventListener("load", function() {
-      imagePreview.setAttribute("src", this.result);
-    });
-    reader.readAsDataURL(file);
-  } else {
-    imagePreview.style.display = null;
-    previewText.style.display = null;
-    imagePreview.setAttribute("src", "");
-  }
+  inpfile.addEventListener("change", function() {
+    const file = this.files[0];
 
-  $("#in").click(function(event) {
-    $("#in").animate({ height: "fadeout", opacity: "toggle" }, "slow");
-    $("#out").animate({ height: "fadeout", opacity: "toggle" }, "slow");
-    $(".login-page").fadeIn(2000);
+    if (file) {
+      const reader = new FileReader();
+      previewText.style.display = "none";
+      imagePreview.style.display = "block";
+      reader.addEventListener("load", function() {
+        imagePreview.setAttribute("src", this.result);
+      });
+      reader.readAsDataURL(file);
+    } else {
+      imagePreview.style.display = null;
+      previewText.style.display = null;
+      imagePreview.setAttribute("src", "");
+    }
   });
 
-  $(".sms a").click(function() {
-    $("form").animate({ height: "toggle", opacity: "toggle" }, "slow");
+  $("#a-upload").click(function(event) {
+    $("#inputImage").click();
   });
 
   event.preventDefault();
