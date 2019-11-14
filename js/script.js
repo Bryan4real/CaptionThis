96705bb0b4d772a6189c7f87e4809a601afe15e8
nessa-waithaka <vanessa.waithaka@gmail.com>
2019-11-14 13:41:22 +0300

    js Get elements image

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
 $(document).ready(function () {
+  const inpfile = document.getElementById("inpImage");
+  const imageContainer = document.getElementById("ImagePreview");
+  const imagePreview = document.getElementById("#image-field");
+  const previewText = document.getElementById(".image-text");
+
+
+
+
+
+
+
+
+
+
 
   $("#in").click(function (event) {
     $("#in").animate({ height: "fadeout", opacity: "toggle" }, "slow");
