11610480b1651c747f89d670771c3bf1c556c843
nessa-waithaka <vanessa.waithaka@gmail.com>
2019-11-14 13:53:34 +0300

    Upload button functionality

--- js/script.js
+++ js/script.js
@@ -1,37 +1,32 @@
-$(document).ready(function () {
+$(document).ready(function() {
   const inpfile = document.getElementById("inpImage");
   const imageContainer = document.getElementById("ImagePreview");
   const imagePreview = document.getElementById("#image-field");
   const previewText = document.getElementById(".image-text");
 
+  if (file) {
+    const reader = new FileReader();
+    previewText.style.display = "none";
+    imagePreview.style.display = "block";
+    reader.addEventListener("load", function() {
+      imagePreview.setAttribute("src", this.result);
+    });
+    reader.readAsDataURL(file);
+  } else {
+    imagePreview.style.display = null;
+    previewText.style.display = null;
+    imagePreview.setAttribute("src", "");
+  }
 
-
-
-
-
-
-
-
-
-
-  $("#in").click(function (event) {
+  $("#in").click(function(event) {
     $("#in").animate({ height: "fadeout", opacity: "toggle" }, "slow");
     $("#out").animate({ height: "fadeout", opacity: "toggle" }, "slow");
     $(".login-page").fadeIn(2000);
-  })
+  });
 
-
-  $(".sms a").click(function () {
+  $(".sms a").click(function() {
     $("form").animate({ height: "toggle", opacity: "toggle" }, "slow");
-  })
-
-
-
-
-
-
-
+  });
 
   event.preventDefault();
-
 });
