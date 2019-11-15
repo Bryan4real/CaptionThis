9a72bf169e06c6891c03578bbacb16bfc1615776
Brian Mwiruki <mwiruki@outlook.com>
2019-11-15 07:21:55 +0300

    color scheme and comments init

--- js/script.js
+++ js/script.js
@@ -1,23 +1,43 @@
-$(document).ready(function () {
+$(document).ready(function() {
+  const inpfile = document.getElementById("inputImage");
+  const imageContainer = document.getElementById("ImagePreview");
+  const imagePreview = document.getElementById("image-field");
+  const previewText = imageContainer.querySelector(".image-text");
 
-  $("#in").click(function (event) {
-    $("#in").animate({ height: "fadeout", opacity: "toggle" }, "slow");
-    $("#out").animate({ height: "fadeout", opacity: "toggle" }, "slow");
-    $(".login-page").fadeIn(2000);
-  })
+  function Capt(text) {
+    this.text = text;
+  }
 
+  inpfile.addEventListener("change", function() {
+    const file = this.files[0];
 
-  $(".sms a").click(function () {
-    $("form").animate({ height: "toggle", opacity: "toggle" }, "slow");
-  })
+    if (file) {
+      const reader = new FileReader();
+      previewText.style.display = "none";
+      imagePreview.style.display = "block";
+      reader.addEventListener("load", function() {
+        imagePreview.setAttribute("src", this.result);
+      });
+      reader.readAsDataURL(file);
+    } else {
+      imagePreview.style.display = null;
+      previewText.style.display = null;
+      imagePreview.setAttribute("src", "");
+    }
+  });
 
+  $("#a-upload").click(function(event) {
+    $("#inputImage").click();
+  });
 
-
-
-
-
-
-
-  event.preventDefault();
-
+  $("#btn_comment").click(function(event) {
+    let caption = $("textarea#textarea").val();
+    captions = new Capt(caption);
+    if (caption != "") {
+      $("#finalorder").prepend(`<tr><td id="pName">` + captions.text + `</tr>`);
+    } else {
+      alert("Please enter caption");
+    }
+    event.preventDefault();
+  });
 });
